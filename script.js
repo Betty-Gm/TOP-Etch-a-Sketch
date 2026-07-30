13ac004d07c554ebad14decaa4619ed1a51f6f7c
bethlehem <bethlehemgebremichael11@gmail.com>
2026-07-30 21:50:33 +0300

    Add progressive square darkening using rgba with each hover

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ resetBtn.addEventListener("click",()=>{
     squareSize= Number(squareSize);
 
     if(squareSize<1 || squareSize>100){
-        alert("Please ente a valid number between 1 and 100");
+        alert("Please enter a valid number between 1 and 100");
         return;
     }
     else{
@@ -26,13 +26,26 @@ function createGrid(size){
         square.style.height= `${newSize}px`;
         square.style.width= `${newSize}px`;
 
-        square.addEventListener("mouseover",()=>{
+        square.style.backgroundColor='white';
 
+        square.dataset.hovercount=0;
+        square.addEventListener("mouseover",()=>{
+            
+            let timesHovered= parseInt(square.dataset.hovercount);
+            if (timesHovered==0){
             let r= Math.floor(Math.random()*256);
             let g= Math.floor(Math.random()*256);
             let b= Math.floor(Math.random()*256);
             
             square.style.backgroundColor= (`rgb(${r}, ${g}, ${b})`);
+            
+            }
+            if(timesHovered<10){
+                timesHovered++;
+                square.style.opacity= (timesHovered*0.1).toString();
+square.dataset.hovercount=timesHovered;
+
+            }
 
         });
 
