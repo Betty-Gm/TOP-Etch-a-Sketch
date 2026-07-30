a273b6cf092340d028feb643410bb0c4d4a5c2a3
bethlehem <bethlehemgebremichael11@gmail.com>
2026-07-30 18:15:34 +0300

    Add button to reset grid with user-defined size, capped at 100

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
+// To create new grid on button click
+const resetBtn= document.getElementById("reset-grid-btn");
+resetBtn.addEventListener("click",()=>{
+    let newSize= prompt("Enter number of squares per side (Max 100): ");
+    newSize= Number(newSize);
+
+    if(newSize<1 || newSize>100){
+        alert("Please ente a valid number between 1 and 100");
+        return;
+    }
+    else{
+        container.textContent="";
+        createGrid(newSize);
+    }
+})
+
 //
+
 const container= document.querySelector(".container");
 
 function createGrid(size){
+    let newSize= 960/size;
     for (let a=0 ; a<size*size; a++){
         const square= document.createElement("div");
         square.classList.add("grid-square");
+        square.style.height= `${newSize}px`;
+        square.style.width= `${newSize}px`;
 
         square.addEventListener("mouseover",()=>{
             square.classList.add("hovered");
