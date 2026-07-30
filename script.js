ce500d0a1413328aa28a90a154049e6905b7ce50
bethlehem <bethlehemgebremichael11@gmail.com>
2026-07-30 17:36:48 +0300

    Add hover effect to leave a trail across the grid

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ function createGrid(size){
     for (let a=0 ; a<size*size; a++){
         const square= document.createElement("div");
         square.classList.add("grid-square");
+
+        square.addEventListener("mouseover",()=>{
+            square.classList.add("hovered");
+
+        });
+
         container.appendChild(square);
     }
 }
